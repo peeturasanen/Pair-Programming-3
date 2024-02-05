baf32bea79994a2d9d2ee3a7c070d5cbbe1130cf
dogface3 <samuel.nousiainen@metropolia.fi>
2024-02-05 14:27:57 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Services from "./components/Services";
-import Tours from "./components/Tours";
-import "./app.css";
+// import About from "./components/About";
+// import Footer from "./components/Footer";
+// import Hero from "./components/Hero";
+// import Navbar from "./components/Navbar";
+// import Services from "./components/Services";
+// import Tours from "./components/Tours";
+// import "./app.css";
+// import NotFound from './components/NotFound';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Navigation from "./components/Navigation";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Navigation />
+//         <Routes>
+//           <Route path="/" element={<Footer />} />
+//           <Route path="/services" element={<Services />} />
+//           <Route path="/tours" element={<Tours />} />
+//           <Route path="/contact" element={<About />} />
+//           <Route path="*" element={<NotFound />} /> {/* Added this line for 404 */}
+//         </Routes>
+//   </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Services  from './components/Services';
+import Tours from './components/Tours';
+import Hero from './components/Hero';
+import About from './components/About';
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <About />
-      <Services />
-      <Tours />
-      <Footer />
-    </div>
-  );
+return (
+<BrowserRouter>
+<Navigation />
+<Routes>
+<Route path="/" element={<Hero />} />
+<Route path="/services" element={<Services />} />
+<Route path="/tours" element={<Tours />} />
+<Route path="/about" element={<About />} />
+</Routes>
+</BrowserRouter>
+);
 }
 
 export default App;
---
+++ src/components/home.js
@@ -0,0 +1,45 @@
+// import Navbar from "./Navbar";
+// import Footer from "./Footer";
+// import Hero from "./Hero";
+// import Header from "./Header";
+// import PageLinks from "./PageLinks";
+// import SocialLinks from "./SocialLinks";
+// import { pageLinks, socialLinks } from "../data";
+
+// const Home = () => {
+//     <div>
+//         <nav className="navbar">
+//             <div className="nav-center">
+//                 <Header />
+//                 <PageLinks parentClass="nav-link" />
+//                 <SocialLinks />
+//             </div>
+//         </nav>
+
+
+//         <footer className="section footer">
+//             <PageLinks parentClass="footer-link" />
+//             <ul className="footer-icons">
+//                 {socialLinks.map((links) => (
+//                     <SocialLink key={links.id} {...links} name="footer-icon" />
+//                 ))}
+//             </ul>
+//             <p className="copyright">
+//                 copyright © travel tours company<span id="date">2023</span>. all rights reserved
+//             </p>
+//         </footer>
+        
+// 			<section className="hero" id="home">
+// 				<div className="hero-banner">
+// 					<h1>backroads app</h1>
+// 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae explicabo debitis est autem dicta.</p>
+// 					<a href="#tours" className="btn hero-btn">
+// 						explore tours
+// 					</a>
+// 				</div>
+// 			</section>
+// 		</div>       
+// };
+
+// export default Home
+
